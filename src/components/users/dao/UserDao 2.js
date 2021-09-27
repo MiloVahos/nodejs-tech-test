4b51caccd85ee8dcadf12cc1ea26810456b22db6
Camilo <milovahos@gmail.com>
2021-09-27 14:34:03 -0500

    Delete duplicated files

--- src/components/users/dao/UserDao 2.js
+++
@@ -1,8 +0,0 @@
-const User = require('../models/UserModel')
-
-const saveUser = async (user) => {
-  const userModel = await User.create(user)
-  return await userModel.get({ plain: true })
-}
-
-module.exports = { saveUser }
--- src/components/users/services/UserService 2.js
+++
@@ -1,8 +0,0 @@
-const { saveUser } = require('../dao/UserDao')
-
-const createNewUser = async (user) => {
-  const newUser = await saveUser(user)
-  return newUser
-}
-
-module.exports = { createNewUser }
