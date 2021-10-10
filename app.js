95e035361925cc1785b2f2a23914ace496697963
Camilo <milovahos@gmail.com>
2021-10-10 16:05:29 -0500

    add mqtt

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const compression = require('compression')
 const mainRouter = require('./src/components/MainRouter')
 const sequelize = require('./src/config/db')
 const { notFoundHandler, errorHandler, wrapErrors } = require('./src/utils/ErrorHandler')
+const mqtt = require('mqtt')
+
+const options = {
+    host: '3132ce987b3746e69dd08006f7615d0c.s1.eu.hivemq.cloud',
+    port: 8883,
+    protocol: 'mqtts',
+    username: 'monostro2@gmail.com',
+    password: 'Camilo1996'
+}
+
+const mqttClient = mqtt.connect(options)
 
 app.use(helment())
 app.use(compression())
@@ -15,6 +26,21 @@ app.use('', mainRouter)
 app.use(notFoundHandler)
 app.use(wrapErrors)
 app.use(errorHandler)
+
+mqttClient.on('connect', function () {
+  console.log('Connected to MQTT broker')
+});
+
+mqttClient.on('error', function (error) {
+  console.log(error)
+});
+
+mqttClient.on('message', function (topic, message) {
+  console.log('Received message:', topic, message.toString())
+});
+
+// subscribe to topic 'my/test/topic'
+mqttClient.subscribe('lean/test')
 
 const server = http.createServer(app)
 sequelize.authenticate().then(() => {
